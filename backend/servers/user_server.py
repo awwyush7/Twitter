a9517dac163b334c352bb44768cbbe6b5339e9b7
Ayush <ayush.pandey@amadeus.com>
2025-04-11 13:25:23 +0530

    added depends, still makes new instantiated classes.

--- backend/servers/user_server.py
+++ backend/servers/user_server.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from backend.models.user import UserInput
 from backend.models.user_collection import UserCollection
 from backend.models.user_login import UserLogin
 from backend.services.login_managment import LoginService
 from backend.services.user_service import UserService
 
-user_service = UserService()
-login_manager = LoginService()
+def get_user_service() -> UserService:
+    return UserService()
+
+def get_login_manager() -> LoginService:
+    return LoginService()
 
 app = FastAPI()
 
@@ -15,26 +18,26 @@ def health() :
     return "Okay"
 
 @app.post("/register")
-async def add (user : UserInput) :
+async def add (user : UserInput, user_service = Depends(get_user_service)) :
     await user_service.add(user)
     return "Ok"
 
 @app.post("/login")
-async def login (user_login_body : UserLogin) :
+async def login (user_login_body : UserLogin, login_manager = Depends(get_login_manager)): 
     token = await login_manager.get_token(user_login_body)
     return token
     
 @app.get("/all")
-async def get_all():
+async def get_all(user_service = Depends(get_user_service)) -> UserCollection:
     users_service = await user_service.get_all()
     return UserCollection(Users=users_service)
     
 @app.get("/delete_database")
-async def delete_all():
+async def delete_all(user_service = Depends(get_user_service)) -> str:
     delete_all_data = await user_service.delete_database()
     return delete_all_data  
 
 @app.get("/get_current_user") 
-def get_current_user():
+def get_current_user(user_service = Depends(get_user_service)) -> UserInput:
     current_user = user_service.get_current_user()
     return current_user
